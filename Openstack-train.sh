e17f0be646efac59553a257ce59d88eb420cce24
Adepurnomo <ade.purnomo50@gmail.com>
2019-12-26 09:02:07 +0700

    Update Openstack-train.sh

--- Openstack-train.sh
+++ Openstack-train.sh
@@ -7,16 +7,29 @@ if [[ $EUID -ne 0 ]]; then
    echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
    exit 1
 fi
-yum update -y
-systemctl stop postfix firewalld NetworkManager
-systemctl disable postfix firewalld NetworkManager
-systemctl mask NetworkManager
-yum remove postfix NetworkManager NetworkManager-libnm -y
-seteforce 0
-getenforce
-sed -i 's/enforcing/disabled/g' /etc/sysconfig/selinux
-yum install centos-release-openstack-train -y
-yum install openstack-packstack -y
+
+echo " wait..."
+yum install curl -y >> /dev/null 2>&1
+mkdir -p /opt/temp
+curl -o /opt/temp/spinner.sh https://raw.githubusercontent.com/tlatsas/bash-spinner/master/spinner.sh >> /dev/null 2>&1
+chmod a+x /opt/temp/spinner.sh
+
+source "/opt/temp/spinner.sh"
+start_spinner '|Update & configure your instance (take several minute. -->'
+sleep 1
+yum update -y >> /dev/null 2>&1
+systemctl stop postfix firewalld NetworkManager >> /dev/null 2>&1
+systemctl disable postfix firewalld NetworkManager >> /dev/null 2>&1
+systemctl mask NetworkManager >> /dev/null 2>&1
+yum remove postfix NetworkManager NetworkManager-libnm -y >> /dev/null 2>&1
+seteforce 0 >> /dev/null 2>&1
+getenforce >> /dev/null 2>&1
+sed -i 's/enforcing/disabled/g' /etc/sysconfig/selinux 
+yum install centos-release-openstack-train -y >> /dev/null 2>&1
+yum install openstack-packstack -y >> /dev/null 2>&1
+cd /opt/temp
+stop_spinner $?
+
 packstack --gen-answer-file=/root/answer.txt
 sed -i 's/CONFIG_NTP_SERVERS=/CONFIG_NTP_SERVERS=0.pool.ntp.org/g' /root/answer.txt
 sed -i 's/CONFIG_KEYSTONE_ADMIN_PW=*/#CONFIG_KEYSTONE_ADMIN_PW=*/g' /root/answer.txt
@@ -25,14 +38,16 @@ sed -i 's/ONFIG_SWIFT_STORAGE_FSTYPE=ext4/ONFIG_SWIFT_STORAGE_FSTYPE=xfs/g' /roo
 sed -i 's/CONFIG_PROVISION_DEMO=y/CONFIG_PROVISION_DEMO=n/g' /root/answer.txt
 sed -i 's/CONFIG_NEUTRON_ML2_MECHANISM_DRIVERS=ovn/CONFIG_NEUTRON_ML2_MECHANISM_DRIVERS=openvswitch/g' /root/answer.txt
 sed -i 's/CONFIG_NEUTRON_L2_AGENT=ovn/CONFIG_NEUTRON_L2_AGENT=openvswitch/g' /root/answer.txt
+
 cat <<EOF>> /root/answer.txt
 CONFIG_KEYSTONE_ADMIN_PW=arumi2507
 CONFIG_MARIADB_PW=arumi2507
 EOF
+
 packstack --answer-file /root/answer.txt
 echo "--------------------"
 echo " password arumi2507 "
 echo "--------------------"
 echo " For your testing on GCP or AWS, put your external ip to /etc/http/conf.d/15-horizon* search 'ServerAlias' " 
-echo " and vnc /etc/nova/nova.conf search 'http://xXx:6080' ad place your external ip "
+echo " and vnc /etc/nova/nova.conf search 'http://xXx:6080' & place your external ip "
 echo " don't forget, enable nested virtualization on your vm instance AWS or GCP "
