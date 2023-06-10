f5b84f22e014e68f50d5f0a97c10398d01ad5bf3
Adepurnomo <ade.purnomo50@gmail.com>
2023-06-10 18:46:18 +0700

    Update Openstack-train.sh

--- Openstack-train.sh
+++ Openstack-train.sh
@@ -20,6 +20,7 @@ systemctl stop NetworkManager >> /dev/null 2>&1
 systemctl disable NetworkManager >> /dev/null 2>&1
 sed -i 's/enforcing/disabled/g' /etc/selinux/config
 yum install python-openstackclient -y >> /dev/null 2>&1
+yum install https://rdoproject.org/repos/rdo-release.rpm -y >> /dev/null 2>&1
 yum install centos-release-openstack-train -y >> /dev/null 2>&1
 yum install openstack-packstack -y >> /dev/null 2>&1
 
